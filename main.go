91504d3d355db6bf976e4a66b43db0c5100bb197
Tõnis Kevvai <tonis@bug.ee>
2021-01-29 01:05:30 +0200

    Sample app added

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	a := App{}
+
+	fmt.Println(os.Getenv("APP_DB_USERNAME"))
+
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"),
+	)
+
+	a.Run(":8010")
+}
